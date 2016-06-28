67536f722ae51eee4f0f6500747b752fd4ab9512
Kyle Kwon <kyleskwon@gmail.com>
2016-06-28 13:36:30 -0400

    add state location and urlrouter providers

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,4 +1,21 @@
-var app = angular.module("embolden", ["firebase"]);
+var app = angular.module('embolden', ['firebase', 'ui.router']);
+
+app.config(['$stateProvider', '$locationProvider', '$urlRouterProvider', function($stateProvider, $locationProvider, $urlRouterProvider) {
+    $locationProvider
+        .html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+    
+    $urlRouterProvider.otherwise("/");
+    
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            controller: 'HomeCtrl',
+            templateUrl: '/templates/home.html'
+        });
+}]);
 
 app.controller("HomeCtrl", function($scope, $firebaseArray, $firebaseAuth) {
 //    var refAuth = new Firebase("https://embolden.firebaseio.com");
